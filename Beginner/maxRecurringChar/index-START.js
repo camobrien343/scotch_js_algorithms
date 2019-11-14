e44f4e1919a7fcdfcacc4716f1aee66bb9fb9924
camobrien343 <camobrien343@gmail.com>
2019-11-14 13:55:53 -0600

    maxRecurringChar complete

--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -5,9 +5,26 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 
 
 
-function maxRecurringChar(text) {
-    // Code goes here
-}
+function maxRecurringChar(str) {
+    str = str.split("");
+    let count = 0, most_recurring = 1, result;
+  
+    for ( let i=0; i<str.length; i++ ) {
+      for ( let j=i; j<str.length; j++) {
+        if ( str[j] === str[i] ) {
+          count++;
+            if ( most_recurring < count ) {
+              most_recurring = count;
+              result = str[i];
+            }
+        }
+      }
+      count = 0;
+    }
+    console.log(`${result} was listed a total of ${most_recurring} times.`)
+    return result;
+  }
+  
 
 
 
