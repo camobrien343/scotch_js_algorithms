a5ba8acf82c9e2022205993d9b6715569f52c393
camobrien343 <camobrien343@gmail.com>
2019-11-16 14:07:44 -0600

    longestWord completed

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -1,12 +1,19 @@
 /* CHALLENGE
-Given a string of text, write an algorithm that returns the text received in a reversed format. 
+Given a string of text, write an algorithm that returns the text received in a reversed format.
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
 
 
 function longestWord(text) {
-    // Code goes here
+    text = text.split(' ');
+    let count = '';
+    for (let i = 0; i < text.length; i++) {
+        if (text[i].length > count.length) {
+            count = text[i];
+        }
+    }
+    return count;
 }
 
 
